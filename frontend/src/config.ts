ebe956919dc3440d83b6705d8e102efdcc2484e1
abstr4cte <abstr4cte@gmail.com>
2025-12-22 01:25:21 +0100

    CORS

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,3 +1,3 @@
 // Konfiguracja aplikacji
-export const API_URL = 'https://api.reklamap.pl/api'
-export const STORAGE_URL = 'https://api.reklamap.pl/storage'
+export const API_URL = '/api'
+export const STORAGE_URL = '/storage'
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,4 +4,16 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://api.reklamap.pl',
+        changeOrigin: true,
+      },
+      '/storage': {
+        target: 'https://api.reklamap.pl',
+        changeOrigin: true,
+      }
+    }
+  }
 })
