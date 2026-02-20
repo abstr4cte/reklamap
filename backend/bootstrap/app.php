aaf2fe857981d0f4b67bb56e61de433b776dc265
abstr4cte <abstr4cte@gmail.com>
2026-02-20 23:22:52 +0100

    corrections

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias(['app.key' => \App\Http\Middleware\VerifyAppKey::class,]);
         $middleware->appendToGroup('api', 'app.key');
+        $middleware->exclude(['/admin/*']);
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
