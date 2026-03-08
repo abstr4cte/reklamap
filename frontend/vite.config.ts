5b5104649cfc3f0100d7487991bb8d7a1116d30b
abstr4cte <abstr4cte@gmail.com>
2026-03-08 22:42:35 +0100

    hash

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,28 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      output: {
+        // Add hash to chunk filenames for cache busting
+        chunkFileNames: 'js/[name]-[hash].js',
+        entryFileNames: 'js/[name]-[hash].js',
+        assetFileNames: (assetInfo) => {
+          const name = assetInfo.name || ''
+          const info = name.split('.')
+          const ext = info[info.length - 1]
+          if (/png|jpe?g|gif|tiff|bmp|ico/i.test(ext)) {
+            return `images/[name]-[hash][extname]`
+          } else if (/woff|woff2|ttf|otf|eot/i.test(ext)) {
+            return `fonts/[name]-[hash][extname]`
+          } else if (ext === 'css') {
+            return `css/[name]-[hash][extname]`
+          }
+          return `assets/[name]-[hash][extname]`
+        }
+      }
+    }
+  },
   server: {
     proxy: {
       '/api': {
